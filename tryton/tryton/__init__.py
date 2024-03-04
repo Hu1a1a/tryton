37d52a8e6ecda36690aa8ec2e52a6414d676e49f
Hulala <yang.ye.1@hotmail.com>
2024-03-04 11:58:41 +0100

    xx

--- tryton/tryton/__init__.py
+++ tryton/tryton/__init__.py
@@ -5,8 +5,8 @@ import locale
 
 import gi
 
-gi.require_version('Gtk', '3.0')
-gi.require_version('Gdk', '3.0')
+gi.require_version('Gtk', '4.0')
+gi.require_version('Gdk', '4.0')
 gi.require_foreign('cairo')
 try:
     gi.require_version('GtkSpell', '3.0')
